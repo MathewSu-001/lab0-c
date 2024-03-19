bb98088b70834fec46452a57febceb8e009e8e2f
MathewSu-001 <su880303@gmail.com>
2024-03-19 16:42:36 +0800

    Modify head file list_sort
    Define cmp unmistakely. Replace struct list_head * to const struct
    list_head.

--- list_sort.h
+++ list_sort.h
@@ -11,6 +11,7 @@ struct list_head;
 
 // typedef int __attribute__((nonnull(2,3))) (*list_cmp_func_t)(void *,
 // 		const struct list_head *, const struct list_head *);
-__attribute__((nonnull)) int cmp(struct list_head *a, struct list_head *b);
+__attribute__((nonnull)) int cmp(const struct list_head *a,
+                                 const struct list_head *b);
 __attribute__((nonnull)) void list_sort(struct list_head *head);
 #endif
